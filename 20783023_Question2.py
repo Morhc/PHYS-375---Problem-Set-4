e663bc00b949d34315438a39d59b385c93026c24
joissa <itsjoshissa@gmail.com>
2022-03-18 14:26:33 -0400

    finished part 2

--- 20783023_Question2.py
+++ 20783023_Question2.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt, rc
 
 rc('text', usetex=True)
 
-def compute_rho_mass(r,spiral=1):
+def compute_rho_and_mass(r,spiral=1):
     """Compute the value of rho and mass using basic ODE solving.
     INPUTS:
         r - The radius to compute rho and mass at.
@@ -14,8 +14,6 @@ def compute_rho_mass(r,spiral=1):
         rho - The computed densities.
         mass - The computed masses.
     """
-
-    #copied from my ODE solver for PS3: theta --> rho | x --> r ish
 
     rho = np.zeros(r.size, dtype=float)
     drho = np.zeros(r.size, dtype=float)
@@ -27,11 +25,13 @@ def compute_rho_mass(r,spiral=1):
 
     dr = r[1] - r[0] #get the r-spacing
     for k in range(1, rho.size):
-        #since drho/dr depended on M, I had to eliminate that
+        #this is from equation (10) in my solutions
         ddrho = -(4*np.pi*rho[k-1]**2 + ((2./r[k]) - (1.0/rho[k-1])*drho[k-1])*drho[k-1])
+        #this is just basic ODE solving technique
         drho[k] = drho[k-1] + ddrho * dr
         rho[k] = rho[k-1] + drho[k] * dr
 
+        #this is from equation (8) in my solutions
         mass[k] = mass[k-1] + 4*np.pi*r[k]**2*rho[k]*dr
 
 
@@ -49,11 +49,7 @@ def part_b(savepath=""):
 
     r = np.linspace(0, 5, 1000)
 
-    rho, mass = compute_rho_mass(r)
-
-    """
-    Solving the dM/dr equation.
-    """
+    rho, mass = compute_rho_and_mass(r)
 
     #exclude the r=0 solution
     plt.plot(r[1:], mass[1:])
@@ -70,10 +66,6 @@ def part_b(savepath=""):
         plt.savefig(savepath.replace('.png', '_mass.png'))
 
     plt.close('all')
-
-    """
-    Solving the dp/dr equation.
-    """
 
     #exclude the r=0 solution
     plt.plot(r[1:], rho[1:])
@@ -110,11 +102,15 @@ def part_c(savepath=""):
     Msun = 1.99e30
 
     r_tilde = np.linspace(0,5,100000)
-    rho_tilde, mass_tilde = compute_rho_mass(r_tilde, spiral=30000)
+    rho_tilde, mass_tilde = compute_rho_and_mass(r_tilde, spiral=30000)
+    #cutting the first index to avoid an error
     rho_tilde, mass_tilde = rho_tilde[1:], mass_tilde[1:]
+
+    #equation (11) in my solutions
     Ps = np.power(k*T/(mu*mp), 4) * 1./(G**3) * np.power(mass_tilde/Msun, 2) * rho_tilde
 
-    r = r_tilde[1:]*G*mu*mp/(k*T)*(Msun/mass_tilde) / AU #converts from unscaled to meters to AU
+    #converts from unscaled to meters to AU, based on equation (5) in my solutions
+    r = r_tilde[1:]*G*mu*mp/(k*T)*(Msun/mass_tilde) / AU
 
     plt.plot(r, Ps)
     #plt.xlim(0, 1e5) #if no spiral
@@ -130,6 +126,51 @@ def part_c(savepath=""):
 
     plt.close('all')
 
+
+def part_d(savepath=""):
+    """Plots the Jeans mass as a function of radius
+    INPUTS:
+        savepath - Path to save the plot to. If nothing is given, it will just display.
+    OUTPUTS:
+        Outputs a plot of Jeans mass as a function of radius
+    """
+    mu = 2.4
+    mp = 1.67e-27
+    G = 6.67e-11
+    k = 1.38e-23
+    AU = 1.496e11
+
+    #our params
+    T = 10
+    Msun = 1.99e30
+
+    r_tilde = np.linspace(0,5,100000)
+
+    _, mass_tilde = compute_rho_and_mass(r_tilde, spiral=1)
+    #cutting the first index to avoid an error
+    mass_tilde = mass_tilde[1:]
+
+    #converts from unscaled to meters to AU, based on equation (5) in my solutions
+    r = r_tilde[1:]*G*mu*mp/(k*T)*(Msun/mass_tilde) / AU
+
+    #from equations (12) and (14) in my solutions
+    mean_rho = 3/(4*np.pi) * Msun/np.power(r, 3)
+    Mj = 0.2*Msun*np.power(T/10, 3/2)*np.power(mean_rho/3e-15, -1/2)
+
+    plt.plot(r, Mj)
+    plt.xlim(0, 1e5) #without spiral
+    plt.ylim(0, 1e15) #without spiral
+    plt.title(r'Jeans Mass $M_J$ vs Radius $r$')
+    plt.ylabel(r'$M_J$ (kg)')
+    plt.xlabel(r'$r$ (AU)')
+    if savepath == "":
+        plt.show()
+    else:
+        plt.savefig(savepath)
+
+    plt.close('all')
+
+
 def main():
 
     here = os.path.dirname(os.path.realpath(__file__))
@@ -139,6 +180,9 @@ def main():
 
     c_path = os.path.join(here, 'PS4-Q2c.png')
     part_c(c_path)
+
+    d_path = os.path.join(here, 'PS4-Q2d.png')
+    part_d(d_path)
 
 if __name__ == "__main__":
     main()
